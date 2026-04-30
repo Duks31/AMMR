ad11b71312f4e67574ac5a9652d2e74e76cc4c9c
chidubem <chidubemjan31@gmail.com>
2026-04-30 22:11:54 +0100

    added camera yaml

--- cika_ws/src/cika_perception/launch/cika_perception.launch.py
+++ cika_ws/src/cika_perception/launch/cika_perception.launch.py
@@ -3,30 +3,25 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node, SetParameter
+from launch_ros.actions import Node
 
 def generate_launch_description():
     perception_dir = get_package_share_directory('cika_perception')
     depthai_driver_dir = get_package_share_directory('depthai_ros_driver')
 
-    # Path to your Luxonis-generated JSON config
-    nn_config_path = os.path.join(perception_dir, 'models', '300_epoch_best.json')
+    # Path to the custom YAML we just created
+    custom_params_file = os.path.join(perception_dir, 'config', 'camera.yaml')
 
     return LaunchDescription([
-        # ── OVERRIDES ────────────────────────────────────────────────────────
-        # This forces the official driver to use our custom pipeline
-        SetParameter(name='camera.i_pipeline_type', value='yolo'),
-        SetParameter(name='nn.i_nn_config_path', value=nn_config_path),
-        SetParameter(name='stereo.i_align_depth', value=True),
-        SetParameter(name='camera.i_enable_pointcloud', value=True),
-
-        # ── NODES ────────────────────────────────────────────────────────────
-        # 1. Official OAK-D Driver (It will now inherit the parameters above)
+        # 1. Official OAK-D Driver, forced to load OUR parameters file
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(depthai_driver_dir, 'launch', 'camera.launch.py')
             ),
-            launch_arguments={'name': 'oak'}.items()
+            launch_arguments={
+                'name': 'oak',
+                'params_file': custom_params_file
+            }.items()
         ),
 
         # 2. Your Custom Inference Translator
