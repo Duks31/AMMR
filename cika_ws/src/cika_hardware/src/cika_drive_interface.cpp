fb020586bd67f0deac0765e238d43d53464c6d42
chidubem <chidubemjan31@gmail.com>
2026-05-19 14:42:27 +0100

    fix add -ve to wheels for viz

--- cika_ws/src/cika_hardware/src/cika_drive_interface.cpp
+++ cika_ws/src/cika_hardware/src/cika_drive_interface.cpp
@@ -201,13 +201,13 @@ namespace cika_hardware
                         {
                             hw_states_[1] = (-p_lf - hw_states_[0]) / dt;
                             hw_states_[3] = (-p_lb - hw_states_[2]) / dt;
-                            hw_states_[5] = (p_rf - hw_states_[4]) / dt;
-                            hw_states_[7] = (p_rb - hw_states_[6]) / dt;
+                            hw_states_[5] = (-p_rf - hw_states_[4]) / dt;
+                            hw_states_[7] = (-p_rb - hw_states_[6]) / dt;
                         }
                         hw_states_[0] = -p_lf;
                         hw_states_[2] = -p_lb;
-                        hw_states_[4] = p_rf;
-                        hw_states_[6] = p_rb;
+                        hw_states_[4] = -p_rf;
+                        hw_states_[6] = -p_rb;
                     }
                 }
                 // ── IMU line ──────────────────────────────────────────────────
