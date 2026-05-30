ed36668e1ed65955da6e7c9873ad558c051b0d64
chidubem <chidubemjan31@gmail.com>
2026-05-30 14:45:12 +0100

    fix-

--- cika_ws/src/cika_bringup/launch/cika_real.launch.py
+++ cika_ws/src/cika_bringup/launch/cika_real.launch.py
@@ -54,7 +54,7 @@ def generate_launch_description():
 
     use_perception_arg = DeclareLaunchArgument(
         name="use_perception",
-        default_value="true",
+        default_value="false",
         choices=["true", "false"],
         description="Launch YOLOv8 inference node",
     )
